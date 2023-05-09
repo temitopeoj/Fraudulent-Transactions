564dfed733c8b28123590c80513447fda0d6934b
temitopeoj <132190127+temitopeoj@users.noreply.github.com>
2023-05-10 01:22:16 +0800

    Add files via upload

--- fraud_trans.sql
+++ fraud_trans.sql
@@ -6,20 +6,25 @@ from fraud_trans;
 
 select type, count('type')
 from fraud_trans
-group by type;
+group by type
+order by count(type) desc;
 
 --Question(2) - which transaction type has the highest number of fraudulent transaction
 select * from fraud_trans;
 
-select type, count ('isfraud')
+select type, count('type')
 from fraud_trans
-where type in ('PAYMENT')
-group by type;
+group by type
+order by count('type') desc
+limit 1;
 
 ---- Questions(3) What is the average fraudulent transaction amount
 select * from fraud_trans;
 
-select avg(amount) from fraud_trans;
+select type, round(avg(amount)) 
+from fraud_trans
+where isfraud = 1
+group by type;
 
 
 -- Question(4) - what is the maximum fraudulent transaction amount
@@ -28,22 +33,29 @@ select * from fraud_trans;
 select max(amount) from fraud_trans;
 
 -- Question(5) What is the minimum fraudulent transaction amount
-select min(amount) from fraud_trans
-where amount > 0;
+select min(amount)
+from fraud_trans;
 
 -- Question(6) Who are the Top 10 customer with highest amount defrauded
 select * from fraud_trans;
 
-select type, amount, nameorig
+select nameorig, sum(amount)
 from fraud_trans
-order by amount desc
+where isfraud = 1
+group by nameorig
+order by sum(amount) desc
 limit 10;
 
 -- Question(7) How effective is the bank in flagging fraud?
--- VERY POOR
-
--- Question(8) Who are the Top 20 fraudster
-select type, amount, namedest, isfraud, isflaggedfraud
+select *
 from fraud_trans
-order by amount desc
+
+select sum(isflaggedfraud) as flagged_as_fraud, sum(isfraud) as fraud, sum(isfraud)/sum(isflaggedfraud)
+from fraud_trans;
+-- Question(8) Who are the Top 20 fraudster
+select namedest, sum(amount) as total_amount
+from fraud_trans
+where isfraud = 1
+group by 1
+order by sum(amount) desc
 limit 20;
